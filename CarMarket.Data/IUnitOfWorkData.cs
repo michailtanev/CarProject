3485f2f6800e597531cf8cb912eef1faf56e7ad9
unknown <madking2010@hotmail.com>
2015-11-19 18:15:11 +0100

    aa

---
+++ CarMarket.Data/IUnitOfWorkData.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CarMarket.Data
+{
+    using CarMarket.Domain;
+
+    public interface IUnitOfWorkData
+    {
+        IRepository<Car> Cars { get; }
+
+        IRepository<Brand> Brands { get; }
+
+        IRepository<CarModel> Models { get; }
+
+        IRepository<User> Users { get; }
+
+        IRepository<Image> Images { get; }
+
+        int SaveChanges();
+    }
+}
---
+++ CarMarket.Models/CarModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMarket.Domain
+{
+    public class CarModel
+    {
+        [Key]
+        public int CarModelId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public int BrandId { get; set; }               //foreign key
+        [Required]
+        public virtual Brand Brand { get; set; }      //Navigation property; virtual for lazy loading
+    }
+}
